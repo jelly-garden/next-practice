eafce78e94855ae2962e6603ee1a2794fbae3285
장정원 <longthegarden@innodep.com>
2022-01-14 14:34:02 +0900

    렌더링 : SSR, SSG

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,36 @@
-export default function Home() {
-    return (
-        <div>
-            <h1>Welcome to My Blog</h1>
-        </div>
-    )
+export default function Home({posts}) {
+  console.log(posts);
+  return (
+      <div>
+          <h1>Welcome to My Blog</h1>
+          <ul>
+            {posts.map(post => (
+              <li key={post.id}>{post.title}</li>
+            ))}
+          </ul>
+      </div>
+  )
+}
+
+// export const getServerSideProps = async() => {
+//     const res = await fetch(`http://localhost:8080/api/posts`)
+//     const posts = await res.json();
+
+//     return {
+//         props: {
+//             posts
+//         }
+//     }
+// }
+
+export const getStaticProps = async() => {
+const res = await fetch(`http://localhost:8080/api/posts`)
+const posts = await res.json();
+
+return {
+    props: {
+        posts
+    },
+    revalidate: 20
+}
 }
