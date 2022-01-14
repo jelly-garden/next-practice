6731fd4875c26d25e365e370aba839b8bdd3bb6c
장정원 <longthegarden@innodep.com>
2022-01-14 15:35:31 +0900

    photo 상세 화면 구현  - staticPaths, staticProps 사용

--- pages/photos.js
+++ pages/photos.js
@@ -1,6 +1,7 @@
 import HeadInfo from "../components/HeadInfo"
 import Image from 'next/image'
 import photosStyles from '../styles/Photos.module.css'
+import Link from 'next/link'
 
 const photos = ({ photos }) => {
     console.log(photos);
@@ -11,13 +12,17 @@ const photos = ({ photos }) => {
             <ul className={photosStyles.photos}>
                 {photos.map(photo => (
                     <li key={photo.id}>
-                        <Image
-                            src={photo.thumbnailUrl}
-                            width={100}
-                            height={100}
-                            alt={photo.title}
-                        />
-                        <span>{photo.title}</span>
+                        <Link href={`/photos/${photo.id}`}>
+                            <a>
+                                <Image
+                                    src={photo.thumbnailUrl}
+                                    width={100}
+                                    height={100}
+                                    alt={photo.title}
+                                />
+                                <span>{photo.title}</span>
+                            </a>
+                        </Link>
                     </li>
                 ))}
             </ul>
